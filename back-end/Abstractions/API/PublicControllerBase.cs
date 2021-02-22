d21222d7f4ae67eeccf1826b8910f5dca11bfe89
Stanislav Shevtsov <ChiefNoir@outlook.com>
2021-02-22 14:27:26 +0300

    [back-end] Added PublicControllerBase

---
+++ back-end/Abstractions/API/PublicControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Abstractions.API
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public abstract class PublicControllerBase : ControllerBase
+    {
+        [HttpGet("ping")]
+        public abstract IActionResult Ping();
+
+        [HttpGet("users")]
+        public abstract Task<IActionResult> GetUsers();
+    }
+}
