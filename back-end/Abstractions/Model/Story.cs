6e2c9fcb687878f9861451ecd9bd773ae9455950
Stanislav Shevtsov <ChiefNoir@outlook.com>
2021-02-22 14:48:33 +0300

    [back-end] Added story model

---
+++ back-end/Abstractions/Model/Story.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Abstractions.Model
+{
+    /// <summary> Story </summary>
+    [ExcludeFromCodeCoverage]
+    public class Story
+    {
+        /// <summary> Unique id or <c>null</c> if story is brand new </summary>
+        public ulong? Id { get; set; }
+
+        /// <summary> Creation date </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary> Title </summary>
+        public string Title { get; set; }
+
+        /// <summary> Subtitle </summary>
+        public string SubTitle { get; set; }
+
+        /// <summary> Content </summary>
+        public StringBuilder Content { get; set; }
+
+        /// <summary> List of <see cref="Tag"/>s</summary>
+        public List<Tag> Tags { get; set; }
+
+        /// <summary> <see cref="Account"/> wich created story </summary>
+        public Account Author { get; set; }
+    }
+}
---
+++ back-end/Abstractions/Model/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Abstractions.Model
+{
+    /// <summary> Tag for <seealso cref="Story"/> </summary>
+    [ExcludeFromCodeCoverage]
+    public class Tag
+    {
+        /// <summary> Unique id or <c>null</c> if tag is brand new </summary>
+        public long? Id { get; set; }
+
+        /// <summary> Unique name </summary>
+        public string Name { get; set; }
+
+        /// <summary> Tag description </summary>
+        public string Description { get; set; }
+    }
+}
